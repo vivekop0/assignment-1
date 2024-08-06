e558185455bb52000e681d384c91a992467d83ff
vivekop0 <sarkarjii534@gamil.com>
2024-08-06 21:51:18 +0530

    asswwwss:

--- components/Button.tsx
+++ components/Button.tsx
@@ -22,7 +22,7 @@ const DropdownButton: React.FC = () => {
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
       >
-        Dropdown button
+        Visit
         <svg
           className="w-2.5 h-2.5 ms-3"
           aria-hidden="true"
@@ -52,38 +52,38 @@ const DropdownButton: React.FC = () => {
           >
             <li>
               <a
-                href="#"
+                href="https://videodubber.ai/translate-video/?ref=producthunt"
                 className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                 onClick={closeDropdown}
               >
-                Dashboard
+                Learn more about website
               </a>
             </li>
             <li>
               <a
-                href="#"
+                href="https://videodubber.ai/translate-video/?ref=producthunt"
                 className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                 onClick={closeDropdown}
               >
-                Settings
+               learn more about VideoDubber
               </a>
             </li>
             <li>
               <a
-                href="#"
+                href="https://videodubber.ai/translate-video/?ref=producthunt"
                 className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                 onClick={closeDropdown}
               >
-                Earnings
+                Learn about the app
               </a>
             </li>
             <li>
               <a
-                href="#"
+                href="https://videodubber.ai/translate-video/?ref=producthunt"
                 className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                 onClick={closeDropdown}
               >
-                Sign out
+                Go to website
               </a>
             </li>
           </ul>
